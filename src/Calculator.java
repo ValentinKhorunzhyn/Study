8ba9a9dc8312fd454c086ace806d214360f00ec4
Valik <valik@example.com>
2018-05-05 19:46:31 +0300

    second

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,6 @@
 public class Calculator {
     public static void main(String[] args) {
         System.out.println("Привет Пидары чмошные");
+        System.out.println("Нет заводу АКБ в Бресте");
     }
 }
