5f6372ae43d5be8c40b3b5f17afe8952ccb96fa3
Valik <valik@example.com>
2018-05-07 15:30:31 +0300

    Calc ready

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
+
 public class Calculator {
+
     public static void main(String[] args) {
-        System.out.println("Привет Пидары чмошные");
-        System.out.println("Нет заводу АКБ в Бресте");
-        System.out.println("Мы за правду");
+        System.out.println("Вас приветствует программа CALCULATOR");
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите первое число: ");
+        int fNumber = in.nextInt();
+        System.out.print("Введите второе число: ");
+        int sNumber = in.nextInt();
+        System.out.print("Введите желаемое арифметическое действие(1- умножить, 2 - сложить," +
+                " 3 - разделить, 4 - вычесть): ");
+        int aAction = in.nextInt();
+        if (aAction == 1) {
+            System.out.println("Ваш ответ: " + (fNumber*sNumber));
+        } else if (aAction == 2) {
+            System.out.println("Ваш ответ: " + (fNumber+sNumber) );
+        } else if (aAction == 3) {
+            System.out.println("Ваш ответ: " + (fNumber/sNumber));
+        } else if (aAction == 4) {
+            System.out.println("Ваш ответ: " + (fNumber-sNumber));
+        } else {
+            System.out.println("Вы вышли за необходимый диапазон");
+        }
     }
 }
