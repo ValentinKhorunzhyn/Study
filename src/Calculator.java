81ea05bb5ccc80c4d921d753cf23d3d8c76c5df3
Valik <valik@example.com>
2018-05-09 16:27:32 +0300

    calculator with OOP

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,25 +3,45 @@ import java.util.Scanner;
 public class Calculator {
 
     public static void main(String[] args) {
-        System.out.println("Вас приветствует программа CALCULATOR");
+        System.out.println("Good Day, I am a CALCULATOR");
+        Calculator calc = new Calculator();
+
+        int fNumber = calc.inputMethod("Input first number please: ");
+        int sNumber = calc.inputMethod("Input second number please: ");
+
+        int aAction = calc.chooseOperation();
+
+        calc.doOperation(fNumber, sNumber, aAction);
+    }
+
+    private int inputMethod(String message) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Введите первое число: ");
-        int fNumber = in.nextInt();
-        System.out.print("Введите второе число: ");
-        int sNumber = in.nextInt();
-        System.out.print("Введите желаемое арифметическое действие(1- умножить, 2 - сложить," +
-                " 3 - разделить, 4 - вычесть): ");
-        int aAction = in.nextInt();
-        if (aAction == 1) {
-            System.out.println("Ваш ответ: " + (fNumber*sNumber));
-        } else if (aAction == 2) {
-            System.out.println("Ваш ответ: " + (fNumber+sNumber) );
-        } else if (aAction == 3) {
-            System.out.println("Ваш ответ: " + (fNumber/sNumber));
-        } else if (aAction == 4) {
-            System.out.println("Ваш ответ: " + (fNumber-sNumber));
-        } else {
-            System.out.println("Вы вышли за необходимый диапазон");
+        System.out.print(message);
+        return in.nextInt();
+    }
+
+    private int chooseOperation() {
+        return this.inputMethod("Enter the arithmetic action (1- multiplication, 2 - addition," +
+                " 3 - division, 4 - subtraction): ");
+    }
+
+    private void doOperation(int fNumber, int sNumber, int aAction) {
+        switch (aAction) {
+            case 1:
+                System.out.println("Your answer: " + (fNumber * sNumber));
+                break;
+            case 2:
+                System.out.println("Your answer: " + (fNumber + sNumber));
+                break;
+            case 3:
+                System.out.println("Your answer: " + (fNumber / sNumber));
+                break;
+            case 4:
+                System.out.println("Your answer: " + (fNumber - sNumber));
+                break;
+            default:
+                System.out.println("You must enter an integer from 1 to 4");
         }
     }
 }
+
