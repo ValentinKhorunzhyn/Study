fd1b58a70b51b8b85ffb59c98b5786f7938b4519
Yury Sauchuk <yury.sauchuk@seavus.com>
2018-05-24 10:51:19 +0300

    Use compare method

--- task05/src/Compare.java
+++ task05/src/Compare.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Compare {
 
-    public Double inputMethod(String message){
+    public Double inputMethod(String message) {
         Scanner in = new Scanner(System.in);
         System.out.print(message);
         Double ipv = in.nextDouble();
@@ -10,53 +10,26 @@ public class Compare {
 
     }
 
-    public void compareMethod(Double v1, Double v2){
-
-        if (v1 > 0 & v2 > 0) {
-            if ((v1 / v2) < 1) {
-                System.out.println("The largest number is: " + v2);
-                System.out.println("The smallest number is: " + v1);
-            } else if ((v1 / v2) > 1) {
+    public void compareMethod(Double v1, Double v2) {
+        int result = Double.compare(v1, v2);
+        switch (result) {
+            case 1: {
                 System.out.println("The largest number is: " + v1);
                 System.out.println("The smallest number is: " + v2);
-            } else {
-                System.out.println("Values are equal to each other");
+                break;
             }
-        } else if (v1 > 0 & v2 < 0) {
-                System.out.println("The largest number is: " + v1);
-                System.out.println("The smallest number is: " + v2);
-        } else if (v1 < 0 & v2 > 0) {
+            case -1: {
                 System.out.println("The largest number is: " + v2);
                 System.out.println("The smallest number is: " + v1);
-        } else if (v1 == 0 || v2 == 0) {
-            if (v1 == 0 & v2 < 0){
-                System.out.println("The largest number is: " + v1);
-                System.out.println("The smallest number is: " + v2);
-            } else if (v1 == 0 & v2 > 0) {
-                System.out.println("The largest number is: " + v2);
-                System.out.println("The smallest number is: " + v1);
-            } else if (v2 == 0 & v1 < 0) {
-                System.out.println("The largest number is: " + v2);
-                System.out.println("The smallest number is: " + v1);
-            } else if (v2 == 0 & v1 > 0) {
-                System.out.println("The largest number is: " + v1);
-                System.out.println("The smallest number is: " + v2);
-            } else if(v1 == 0 & v2 == 0) {
-                System.out.println("Values are equal to each other");
+                break;
             }
-        } else {
-            if ((Math.abs(v1)/Math.abs(v2)) < 1) {
-                System.out.println("The largest number is: " + v1);
-                System.out.println("The smallest number is: " + v2);
-            } else if ((Math.abs(v1)/Math.abs(v2)) > 1) {
-                System.out.println("The largest number is: " + v2);
-                System.out.println("The smallest number is: " + v1);
-            } else {
-                System.out.println("Values are equal to each other");
+            default: {
+                System.out.println("v2 equals v1");
+                break;
             }
         }
     }
-    }
+}
 
 
 
