7b0997208620961eb79a5daf2fce9837fd1400cf
Valik <valik@example.com>
2018-05-24 22:00:55 +0300

    task06 commit1

---
+++ task06/src/PrimeNumbers.java
@@ -0,0 +1,24 @@
+public class PrimeNumbers {
+
+
+        public void Find(){
+        boolean flag = false;
+
+        for (int i = 2; i <= 150; i++) {
+
+            for (int j = 2; j < i; j++) {
+
+                if (i % j != 0) {
+                    flag = true;
+                } else {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                System.out.print(" " + i + " ");
+            }
+        }
+        }
+
+    }
