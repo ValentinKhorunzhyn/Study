e5d8c88242d001fcb28c2a2e45f749d6c7d8fb72
Valik <valik@example.com>
2018-06-02 21:59:54 +0300

    task10 commit1

---
+++ task10/src/Binary.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Binary {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n = in.nextInt();
+        StringBuilder sb = new StringBuilder();
+        while (n != 0){
+
+           int o = n%2;
+           Character.toChars(o);
+           sb.append(o);
+           n = n/2;
+        }
+        sb.reverse();
+        System.out.println(sb);
+    }
+}
