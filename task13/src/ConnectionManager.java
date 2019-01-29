0495354d506bdf2393ada4f99c7fa90d5cb51574
Valik <messuri.87@mail.ru>
2019-01-29 17:26:03 +0300

    task 13 commit

--- task13/src/ConnectionManager.java
+++ task13/src/ConnectionManager.java
@@ -1,34 +1,29 @@
 public class ConnectionManager {
 
-    String name;
-    static Connection[]Manager = new Connection[5];
+    static private Connection[] manager = new Connection[5];
 
-    public static void main(String[] args) {
-        Manager[0] = new Connection("Luka Modrich");
-        Manager[1] = new Connection("Ivan Perishich");
-        Manager[2] = new Connection("Ivan Rakitich");
-        Manager[3] = new Connection("Mateo Kovachich");
-        Manager[4] = new Connection("Domagoi Vida");
-
-        
+    static {
+        manager[0] = new Connection("Luka Modrich");
+        manager[1] = new Connection("Ivan Perishich");
+        manager[2] = new Connection("Ivan Rakitich");
+        manager[3] = new Connection("Mateo Kovachich");
+        manager[4] = new Connection("Domagoi Vida");
 
 
     }
 
-    private static Object getObject(String name){
-        return Object.class;
-    }
-
-
-
-    private static class Connection {
-
-        String name;
-
-        public Connection(String name){
-
-            this.name = name;
+    static Connection getConnection() {
+        Connection connection = null;
+        for (int i = 0; i < manager.length; i++) {
+            if (manager[i] != null) {
+                connection = manager[i];
+                manager[i] = null;
+                return connection;
+            }
         }
-
+        return connection;
     }
 }
+
+
+
