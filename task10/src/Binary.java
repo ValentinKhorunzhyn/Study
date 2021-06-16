dc156adb7938bbb0d4f3653fb6fce5abe20af6e7
Valik <messuri.87@mail.ru>
2021-06-16 12:47:15 +0300

    1

--- task10/src/Binary.java
+++ task10/src/Binary.java
@@ -7,13 +7,13 @@ public class Binary {
         System.out.print("Enter the number: ");
         int n = in.nextInt();
         StringBuilder sb = new StringBuilder();
-        while (n != 0){
-
-           int o = n%2;
-           sb.append(o);
-           n = n/2;
+        while (n != 0) {
+            int o = n % 2;
+            sb.append(o);
+            n = n / 2;
         }
         sb.reverse();
+        System.out.println(sb);
         System.out.println(sb);
     }
 }
